f210e0232f23d62bf4c3e67cfa030cfe5168a806
behzodgayratoff <60029111+behzodgayratoff@users.noreply.github.com>
2021-04-16 00:25:14 +0500

    lab#6 is done

--- webpage/index.php
+++ webpage/index.php
@@ -1,31 +1,162 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<title>Validating Forms</title>
-		<link href="style.css" type="text/css" rel="stylesheet" />
-	</head>
-	
-	<body>
-		<h1>Registration Form</h1>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>validation forms</title>
+</head>
+<body>
+<?php
 
-		<p>
-			This form validates user input and then displays "Thank You" page.
-		</p>
-		
-		<hr />
-		
-		<h2>Please, fill below fields correctly</h2>
-		<dl>
-			<dt>Name</dt>
-			<dd>
-				???
-			</dd>
-			
-			<!-- Write other fiels similar to Name as specified in lab6.pdf -->
-		</dl>
-		
-		<div>
-			Register
-		</div>
-	</body>
+$name = "";
+$email = "";
+$username = "";
+$address = "";
+$city = "";
+$postalCode = "";
+$phone = "";
+$mobile = "";
+$credit = "";
+$salary = "";
+
+$isNameValid = true;
+$isEmailValid = true;
+$isUserNameValid = true;
+$isAddressValid = true;
+$isCityValid = true;
+$isPostalCodeValid = true;
+$isPhoneValid = true;
+$isMobileValid = true;
+$isCreditValid = true;
+$isSalaryValid = true;
+
+if ($_SERVER["REQUEST_METHOD"] == 'POST')
+{
+    $name = $_REQUEST['name'];
+    $email = $_REQUEST['email'];
+    $username = $_REQUEST['username'];
+    $address = $_REQUEST['address'];
+    $city = $_REQUEST['city'];
+    $postalCode = $_REQUEST['postalCode'];
+    $phone = $_REQUEST['phone'];
+    $mobile = $_REQUEST['mobile'];
+    $credit = $_REQUEST['credit'];
+    $salary = $_REQUEST['salary'];
+
+    $isNameValid = preg_match('/^([A-Z][a-z][a-z \-]{2,})*([a-z]{2,})$/i', $name);
+    $isEmailValid = preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/', $email);
+    $isUserNameValid = preg_match('/^([a-z][a-z \-]{2,})*([a-z]{5,})$/i', $username);
+    $isAddressValid = preg_match('/^([a-z][a-z \-]{2,})*([a-z]{5,})$/i', $address);
+    $isCityValid = preg_match('/^([a-z][a-z \-]{2,})*([a-z]{5,})$/i', $city);
+    $isPostalCodeValid = preg_match('/([\d \-+()]{6,})+$/', $postalCode);
+    $isPhoneValid = preg_match('/([\d \-+()]{9,})+$/', $phone);
+    $isMobileValid = preg_match('/([\d \-+()]{9,})+$/', $mobile);
+    $isCreditValid = preg_match('/([\d \-+()]{16,})+$/', $credit);
+    $isSalaryValid = preg_match('/[UZS][ \d \-+()]+$/', $salary);
+
+    $isValid = $isNameValid && $isEmailValid && $isUserNameValid && $isAddressValid && $isCityValid && $isPostalCodeValid && $isPhoneValid && $isMobileValid && $isCreditValid && $isSalaryValid;
+    if ($isValid)
+    {
+        header('Location: thanks.php', TRUE, 301);
+    }
+}
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>validation forms</h1>
+            <form action="index.php" method="post">
+                <div class="mb-3">
+                    <label for="name" class="form-label">name</label>
+                    <input type="text" class="form-control <?= $isNameValid?'':'is-invalid' ?>" id="name" name="name" value="<?= $name ?>" placeholder="please! enter name">
+                    <div class="invalid-feedback">
+                        please! enter name
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">email</label>
+                    <input type="text" class="form-control <?= $isEmailValid?'':'is-invalid' ?>" id="email" name="email" value="<?= $email ?>" placeholder="please! enter email">
+                    <div class="invalid-feedback">
+                        please! enter email
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="username" class="form-label">username</label>
+                    <input type="text" class="form-control <?= $isUserNameValid?'':'is-invalid' ?>" id="username" name="username" value="<?= $username ?>" placeholder="please! enter username">
+                    <div class="invalid-feedback">
+                        please! enter username
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="address" class="form-label">address</label>
+                    <input type="text" class="form-control <?= $isAddressValid?'':'is-invalid' ?>" id="address" name="address" value="<?= $address ?>" placeholder="please! enter address">
+                    <div class="invalid-feedback">
+                        please! enter address
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="city" class="form-label">city</label>
+                    <input type="text" class="form-control <?= $isCityValid?'':'is-invalid' ?>" id="city" name="city" value="<?= $city ?>" placeholder="please! enter city">
+                    <div class="invalid-feedback">
+                        please! enter city
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="postalCode" class="form-label">postal code</label>
+                    <input type="text" class="form-control <?= $isPostalCodeValid?'':'is-invalid' ?>" id="postalCode" name="postalCode" value="<?= $postalCode ?>" placeholder="please! enter postal code">
+                    <div class="invalid-feedback">
+                        please! enter postal code
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="phone" class="form-label">home phone</label>
+                    <input type="text" class="form-control <?= $isPhoneValid?'':'is-invalid' ?>" id="phone" name="phone" value="<?= $phone ?>" placeholder="please! enter phone number">
+                    <div class="invalid-feedback">
+                        please! enter phone number
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="mobile" class="form-label">mobile phone</label>
+                    <input type="text" class="form-control <?= $isMobileValid?'':'is-invalid' ?>" id="mobile" name="mobile" value="<?= $mobile ?>" placeholder="please! enter mobile phone number">
+                    <div class="invalid-feedback">
+                        please! enter mobile phone number
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="credit" class="form-label">credit card</label>
+                    <input type="text" class="form-control <?= $isCreditValid?'':'is-invalid' ?>" id="credit" name="credit" value="<?= $credit ?>" placeholder="please! enter credit card number">
+                    <div class="invalid-feedback">
+                        please! enter credit card number
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="salary" class="form-label">salary</label>
+                    <input type="text" class="form-control <?= $isSalaryValid?'':'is-invalid' ?>" id="salary" name="salary" value="<?= $salary ?>" placeholder="please! enter salary">
+                    <div class="invalid-feedback">
+                        please! enter salary
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <input type="submit" class="btn btn-primary" value="submit">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+</body>
 </html>
--- webpage/regex_valid_form.php
+++ webpage/regex_valid_form.php
@@ -1,52 +1,115 @@
 <?php
 
-	$pattern="";
-	$text="";
-	$replaceText="";
-	$replacedText="";
-
-	$match="Not checked yet.";
+$email = "";
+$mail="it is not checked yet";
 
 if ($_SERVER["REQUEST_METHOD"]=="POST") {
-	$pattern=$_POST["pattern"];
-	$text=$_POST["text"];
-	$replaceText=$_POST["replaceText"];
 
-	$replacedText=preg_replace($pattern, $replaceText, $text);
-
-	if(preg_match($pattern, $text)) {
-						$match="Match!";
-					} else {
-						$match="Does not match!";
-					}
+    $email = $_REQUEST['email'];
+    if (preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/', $email)) {
+        $mail = "email is valid";
+    } else {
+        $mail = "email is not valid";
+    }
 }
 
-?>
+$phone = "";
+$number="it is not checked yet";
+if ($_SERVER["REQUEST_METHOD"]=="POST") {
 
+    $phone = $_REQUEST['phone'];
+    if (preg_match('/[+][\d \-+()]+$/', $phone)) {
+        $number = "phone number is valid";
+    } else {
+        $number = "phone number is not valid";
+    }
+}
+
+$str = "";
+$removed ="";
+
+if ($_SERVER["REQUEST_METHOD"]=="POST") {
+
+    $str = $_REQUEST['str'];
+    $removed = str_replace(' ', '', $str);
+}
+
+$non_numeric = "";
+$non_numeric_removed = "";
+
+if ($_SERVER["REQUEST_METHOD"]=="POST") {
+
+    $non_numeric = $_REQUEST['non_numeric'];
+    $non_numeric_removed = preg_replace('/^[^0-9]/', '', $non_numeric);
+}
+
+$task = "";
+$new_line = "";
+
+if ($_SERVER["REQUEST_METHOD"]=="POST") {
+
+    $task = $_REQUEST['task'];
+    $new_line = preg_replace('/\r | \n/', '', $task);
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Valid Form</title>
+    <link href="style.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
+
+
 	<form action="regex_valid_form.php" method="post">
-		<dl>
-			<dt>Pattern</dt>
-			<dd><input type="text" name="pattern" value="<?= $pattern ?>"></dd>
 
-			<dt>Text</dt>
-			<dd><input type="text" name="text" value="<?= $text ?>"></dd>
+        <dl>
+            <dt>email</dt>
+            <dd>
+                <label>
+                    <input type="email" name="email" value="<?= $email ?>">
+                </label>
+            </dd>
+            <dt>output email</dt>
+            <dd><?=	$mail ?></dd>
 
-			<dt>Replace Text</dt>
-			<dd><input type="text" name="replaceText" value="<?= $replaceText ?>"></dd>
 
-			<dt>Output Text</dt>
-			<dd><?=	$match ?></dd>
 
-			<dt>Replaced Text</dt>
-			<dd> <code><?=	$replacedText ?></code></dd>
+            <dt>phone</dt>
+            <dd>
+                <label>
+                    <input type="text" name="phone" value="<?= $phone ?>">
+                </label>
+            </dd>
+            <dt>output number</dt>
+            <dd><?=	$number ?></dd>
+
+
+
+            <dt>string</dt>
+            <dd>
+                <label>
+                    <input type="text" name="str" value="<?= $removed ?>">
+                </label>
+            </dd>
+
+
+            <dt>non-numeric</dt>
+            <dd>
+                <label>
+                    <input type="text" name="non_numeric" value="<?= $non_numeric_removed ?>">
+                </label>
+            </dd>
+
+
+            <dt>new-line</dt>
+            <dd>
+                <label>
+                    <input type="text" name="task" value="<?= $new_line ?>">
+                </label>
+            </dd>
 
 			<dt>&nbsp;</dt>
 			<dd><input type="submit" value="Check"></dd>
